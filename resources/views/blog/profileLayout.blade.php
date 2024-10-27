9061ba87ba79be5b79934109db36dfef04ddf311
Dalbabon <gusarovpol777@mail.ru>
2024-10-28 00:47:28 +0400

    UralUndergroundNuans2.0

--- resources/views/blog/profileLayout.blade.php
+++ resources/views/blog/profileLayout.blade.php
@@ -14,10 +14,11 @@
     <title>muse</title>
 </head>
 <body>
-    <div class="container-fluid">
+    <div class="container-fluid" style="background-image: url({{asset('images/m1000x1000.jpg')}}); background-size: cover;  ">
         <div class="row align-items-start" style="justify-content: space-between">
             <div class="col align-self-start" style="width:fit-content; padding-left:0">
-                <div class="container h-auto w-auto" style="background-image: url({{asset('images/m1000x1000.jpg')}}); background-size: contain; margin-right:0">
+                {{-- "background-image: url({{asset('images/backgroundCustom1.png')}}); background-size: contain; --}}
+                <div class="container h-auto w-auto" style= "margin-right:0">
                     <div class="container">
                         <a href="/" class="logo fs-4 mx-4">muse.</a>
                         <div class="container d-flex" style="justify-content: center; align-items: center; flex-direction: column; color: var(--white)">
